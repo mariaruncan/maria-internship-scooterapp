4cc3397585f338e61d08db53fe88a22c7a3a6902
mariaruncan <87378430+mariaruncan@users.noreply.github.com>
2022-08-11 18:44:28 +0300

    navigate to onboarding fragment and add get started button

--- app/src/main/java/com/internship/move/feature/home/HomeFragment.kt
+++ app/src/main/java/com/internship/move/feature/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.internship.move.feature.onboarding
+package com.internship.move.feature.home
 
 import android.os.Bundle
 import android.view.View
--- app/src/main/java/com/internship/move/feature/splash/SplashFragment.kt
+++ app/src/main/java/com/internship/move/feature/splash/SplashFragment.kt
@@ -1,17 +1,20 @@
 package com.internship.move.feature.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.internship.move.R
-import com.internship.move.databinding.FragmentSplashBinding
-import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
 
-    private val binding by viewBinding(FragmentSplashBinding::bind)
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToOnboardingGraph())
+        }, 2000)
     }
 }
