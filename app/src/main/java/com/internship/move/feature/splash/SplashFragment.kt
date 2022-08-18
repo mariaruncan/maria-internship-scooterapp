d674467cfc7f0b5cf1762c72561c8b0781abf136
mariaruncan <87378430+mariaruncan@users.noreply.github.com>
2022-08-18 14:20:23 +0300

    resolve bug when splash fragment goes in foreground

--- app/src/main/java/com/internship/move/feature/splash/SplashFragment.kt
+++ app/src/main/java/com/internship/move/feature/splash/SplashFragment.kt
@@ -13,33 +13,43 @@ import com.internship.move.feature.onboarding.OnboardingFragmentDirections
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            val sharedPref = requireActivity().getSharedPreferences(
-                getString(R.string.preference_file_key), Context.MODE_PRIVATE
+    private val myHandler: Handler = Handler(Looper.getMainLooper())
+    private val myRunnable: Runnable = kotlinx.coroutines.Runnable {
+        val sharedPref = requireActivity().getSharedPreferences(
+            getString(R.string.preference_file_key), Context.MODE_PRIVATE
+        )
+        val all = sharedPref.all
+        val hasVisitedOnboarding = sharedPref.getBoolean(
+            getString(R.string.has_visited_onboarding_key), false
+        )
+        if (!hasVisitedOnboarding) {
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToOnboardingGraph())
+        } else {
+            val hasVisitedAuthentication = sharedPref.getBoolean(
+                getString(R.string.has_visited_authentication_key), false
             )
-            val all = sharedPref.all
-            val hasVisitedOnboarding = sharedPref.getBoolean(
-                getString(R.string.has_visited_onboarding_key), false
-            )
-            if (!hasVisitedOnboarding) {
-                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToOnboardingGraph())
+            if (!hasVisitedAuthentication) {
+                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToAuthenticationGraph())
             } else {
-                val hasVisitedAuthentication = sharedPref.getBoolean(
-                    getString(R.string.has_visited_authentication_key), false
-                )
-                if (!hasVisitedAuthentication) {
-                    findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToAuthenticationGraph())
-                } else {
-                    findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeGraph())
-                }
+                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeGraph())
             }
-        }, SPLASH_FRAGMENT_DELAY)
+        }
     }
 
+    override fun onPause() {
+        super.onPause()
+
+        myHandler.removeCallbacks(myRunnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        myHandler.postDelayed(myRunnable, SPLASH_FRAGMENT_DELAY);
+    }
+
+
     companion object {
-        private const val SPLASH_FRAGMENT_DELAY = 1500L
+        private const val SPLASH_FRAGMENT_DELAY = 2000L
     }
 }
