343dbc80316b14b5020afcfaed57f9be7651a3e3
mariaruncan <87378430+mariaruncan@users.noreply.github.com>
2022-08-22 17:16:18 +0300

    add clickable text

--- app/src/main/java/com/internship/move/feature/authentication/RegisterFragment.kt
+++ app/src/main/java/com/internship/move/feature/authentication/RegisterFragment.kt
@@ -3,12 +3,14 @@ package com.internship.move.feature.authentication
 import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.internship.move.R
 import com.internship.move.databinding.FragmentRegisterBinding
 import com.internship.move.util.Constants.SharedPref.KEY_APP_PREFERENCES
 import com.internship.move.util.Constants.SharedPref.KEY_HAS_VISITED_AUTHENTICATION
+import com.internship.move.util.addClickableSpan
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
@@ -18,6 +20,17 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initListeners()
+
+        binding.termsPrivacyTV.addClickableSpan("Terms and Conditions") {
+            Toast.makeText(context, "Terms and Conditions", Toast.LENGTH_SHORT).show()
+        }
+        binding.termsPrivacyTV.addClickableSpan("Privacy Policy") {
+            Toast.makeText(context, "Privacy Policy", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initListeners() {
         binding.homeBtn.setOnClickListener {
             updateSharedPreferences()
             findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToHomeGraph())
--- app/src/main/java/com/internship/move/util/extensions.kt
+++ app/src/main/java/com/internship/move/util/extensions.kt
@@ -1,7 +1,34 @@
 package com.internship.move.util
 
+import android.text.Selection
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.TextView
 
-fun TextView.dada(text: String, function: () -> Unit) {
+fun TextView.addClickableSpan(text: String, callback: View.OnClickListener) {
+    val spannableString = SpannableString(this.text)
+    var startIndexOfLink = -1
 
+    val clickableSpan = object : ClickableSpan() {
+        override fun updateDrawState(textPaint: TextPaint) {
+            super.updateDrawState(textPaint)
+            textPaint.isUnderlineText = true
+        }
+
+        override fun onClick(view: View) {
+            Selection.setSelection((view as TextView).text as Spannable, 0)
+            callback.onClick(view)
+        }
+    }
+    startIndexOfLink = this.text.toString().indexOf(text, startIndexOfLink + 1)
+    spannableString.setSpan(clickableSpan, startIndexOfLink, startIndexOfLink + text.length, SPAN_EXCLUSIVE_EXCLUSIVE)
+
+
+    this.movementMethod = LinkMovementMethod.getInstance() // without LinkMovementMethod, link can not click
+    this.setText(spannableString, TextView.BufferType.SPANNABLE)
 }
