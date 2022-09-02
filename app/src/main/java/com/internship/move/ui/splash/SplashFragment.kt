f23cf335d5c37b1ba20d4d841272d384dd59afc8
mariaruncan <87378430+mariaruncan@users.noreply.github.com>
2022-09-02 10:39:04 +0300

    use coroutines instead of handler class

--- app/src/main/java/com/internship/move/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/internship/move/ui/splash/SplashFragment.kt
@@ -1,37 +1,38 @@
 package com.internship.move.ui.splash
 
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.internship.move.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
 
     private val viewModel: SplashViewModel by viewModel()
-    private val navigationHandler: Handler = Handler(Looper.getMainLooper())
-    private val navigationRunnableJob: Runnable = Runnable {
-        val hasVisitedOnboarding = viewModel.getHasViewedOnboarding()
-        val isLoggedIn = viewModel.getIsLoggedIn()
+    private lateinit var job: Job
 
-        when {
-            hasVisitedOnboarding and isLoggedIn -> findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeGraph())
-            hasVisitedOnboarding -> findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToAuthenticationGraph())
-            else -> findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToOnboardingGraph())
+    override fun onResume() {
+        super.onResume()
+        job = CoroutineScope(Dispatchers.Main).launch {
+            delay(SPLASH_FRAGMENT_DELAY)
+            val hasVisitedOnboarding = viewModel.getHasViewedOnboarding()
+            val isLoggedIn = viewModel.getIsLoggedIn()
+
+            when {
+                hasVisitedOnboarding and isLoggedIn -> findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeGraph())
+                hasVisitedOnboarding -> findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToAuthenticationGraph())
+                else -> findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToOnboardingGraph())
+            }
         }
     }
 
     override fun onPause() {
         super.onPause()
-
-        navigationHandler.removeCallbacks(navigationRunnableJob)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        navigationHandler.postDelayed(navigationRunnableJob, SPLASH_FRAGMENT_DELAY)
+        job.cancel()
     }
 
     companion object {
